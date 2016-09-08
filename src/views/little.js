311180ab0315503832c5a88cf798781dee10855a
Hu <howard.hu@BJGLPR-NMX0032.local>
2016-09-08 10:44:45 +0800

    simple trigger and listenTo

--- src/views/little.js
+++ src/views/little.js
@@ -6,14 +6,15 @@ import _ from 'underscore';
 import Backbone from 'backbone';
 import Marionette from 'backbone.marionette';
 
-
-export var littleView = Marionette.LayoutView.extend({  // 2
+var littleView
+export default littleView = Marionette.LayoutView.extend({  // 2
     template: require('../templates/little.html') , // 4
 
     onRender:function(){
         debugger
     },
     onShow:function(){
+
 
         debugger
 
